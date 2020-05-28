3a31411330f3474eee7bc63d47b6631908002030
Nicolas R <rudasn@gmail.com>
2020-05-28 17:28:21 +0300

    Release 0.1.0

---
+++ precache-manifest.a85cbfe50406fb3cee595768dab9c343.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc90c8d62e4b197039504029bf8a781",
+    "url": "https://2020.afrobananarepublic.com/index.html"
+  },
+  {
+    "revision": "bac6566466bd167aa0d4",
+    "url": "https://2020.afrobananarepublic.com/static/css/2.e611564a.chunk.css"
+  },
+  {
+    "revision": "1f4ebbd510e2d5a01643",
+    "url": "https://2020.afrobananarepublic.com/static/css/main.47ce6b1b.chunk.css"
+  },
+  {
+    "revision": "bac6566466bd167aa0d4",
+    "url": "https://2020.afrobananarepublic.com/static/js/2.5defa8a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://2020.afrobananarepublic.com/static/js/2.5defa8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f4ebbd510e2d5a01643",
+    "url": "https://2020.afrobananarepublic.com/static/js/main.ede7bebb.chunk.js"
+  },
+  {
+    "revision": "212248f80da53901b66e",
+    "url": "https://2020.afrobananarepublic.com/static/js/runtime-main.a9d9d9b9.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "https://2020.afrobananarepublic.com/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "https://2020.afrobananarepublic.com/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "https://2020.afrobananarepublic.com/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "https://2020.afrobananarepublic.com/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "https://2020.afrobananarepublic.com/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "1875ecb11b9eb5604a8dd416e6006431",
+    "url": "https://2020.afrobananarepublic.com/static/media/poster-shared.1875ecb1.png"
+  },
+  {
+    "revision": "1875ecb11b9eb5604a8dd416e6006431",
+    "url": "https://2020.afrobananarepublic.com/static/media/poster.1875ecb1.png"
+  }
+]);
